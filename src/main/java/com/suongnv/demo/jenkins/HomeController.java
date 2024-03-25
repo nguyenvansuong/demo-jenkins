d92705dd1624d6b16e99e195ce65fe0365746e39
suongnv <suongnguyenytm2001@gmail.com>
2024-03-25 22:56:38 +0700

    logger

--- src/main/java/com/suongnv/demo/jenkins/HomeController.java
+++ src/main/java/com/suongnv/demo/jenkins/HomeController.java
@@ -1,14 +1,17 @@
 package com.suongnv.demo.jenkins;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HomeController {
 
     @GetMapping("/greeting")
     public String getGreeting(@RequestParam(defaultValue = "world") String name) {
+        log.info("name: {}", name);
         return "Hello " + name;
     }
 }
